64d48bf2e263f38eca5b1b81ea121042c72543f1
timblack330 <136918834+timblack330@users.noreply.github.com>
2023-06-18 14:44:09 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -36,6 +36,8 @@
    <head>
       <meta charset="utf-8" />
       <link rel="stylesheet" href="style.css" />
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <script type="js/bootstrap.js"></script>
    </head>
    <body>
    <div class="logo"><img src="logo.png"></div>

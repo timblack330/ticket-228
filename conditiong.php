31a85c6361e3dc3ff6818fb415da0b4efe78d2fd
timblack330 <blazingsoul4@gmail.com>
2023-07-21 19:36:19 +0200

    nouveau

---
+++ conditiong.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Conditions Generales</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+        <body>
+        <script src="jquery-3.5.1.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Debut -->
+
+<!-- Navbar debut-->
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="Index.php">
+          <img src="logo4.png" width="300" height="80" alt="">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Se connecter</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="inscription.php">S'inscrire</a>
+        </li>
+      </ul>
+      <!--       <div class="d-flex">
+                <ul>
+                    <li><span class="social-icon social-facebook"><i class="fa fa-facebook"></i></span></li>
+                    <li><span class="social-icon social-google"><i class="fa fa-google"></i></span></li>
+                    <li><span class="social-icon social-linkedin"><i class="fa fa-linkedin"></i></span></li>
+                    <li><span class="social-icon social-instagram"><i class="fa fa-instagram"></i></span></li>
+                    <li><span class="social-icon social-twitter"><i class="fa fa-twitter"></i></span></li>
+                </ul>
+            </div> -->
+</nav>
+    <h2>Informations légales</h2>
+    
+    <p style="text-align:center">MENTIONS LEGALES :</p>
+    
+    <p style="text-align:justify">Correspondant aux dispositions  des articles 6-III et 19 de la Loi n* 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous informons les utilisateurs du site web : <a href="https://ticket+.tg" target="_blank">ticket+.tg</a> Ces infos qui suivent :</p>
+    <br />
+    <p style="text-align:justify"><strong>1. Données légales :</strong></p>
+
+        
+    <p style="text-align:justify">Le Propriétaire est : <strong>Timothee DJATO</strong><br />
+    Adresse physique du titulaire : <strong>**** BP - Lome</strong><br />
+    <br />
+    Le Développeur du site est : <strong>AGBEKPENOU Marius</strong><br />
+    Le Responsable de la publication : <strong>AGBEKPENOU Marius</strong><br />
+    Par quel moyen joindre le Responsable de la publication : <strong>****</strong><br />
+    <br />
+    Le Concepteur est : <strong>AGBEKPENOU Marius</strong><br />
+        Comment contacter le Réalisateur : <strong><a href="mailto:****?subject=Contact a partir des mentions légales via le site ticket+.tg">****</a></strong><br />
+    <br />
+
+    Le site est hebergé par : Autre<br />
+    **** au capital social de ****<br />
+    Registre du commerce et des sociétés (RCS) ****<br />
+    Code APE (NAF) ****<br />
+    Numero TVA : 18<br />
+    Siège Sociale de l'hébergeur : ****<br />
+    <br />
+    <br />
+    Les Mentions Légales ont été réalisées via <strong><a href="https://www.generateur-de-mentions-legales.com" target="_blank">cet outil</a></strong><br />
+
+    <p style="text-align:justify"></p>
+    
+
+    
+    
+    <p style="text-align:justify"></p>
+    <br />
+    <p style="text-align:justify"><strong>2. Introduction :</strong></p>
+    
+    <p style="text-align:justify">Tout internaute qui utilise le site web : <a href="https://ticket+.tg" target="_blank">ticket+.tg</a> est considéré comme usageé du site web.<br />
+    Le site suivant <strong>ticket+.tg</strong> réuni différents services, en l'état, disponible pour les utilisateur. Il est naturellement précisé que ces derniers doivent rester honnête et faire preuve de bonne foi tant envers les différents utilisateurs qu'envers le Réalisateur du site ticket+.tg. Le site ticket+.tg est actualisé régulièrement par AGBEKPENOU Marius.<br />
+    AGBEKPENOU Marius s'efforce d'indiqué sur le site ticket+.tg des informations les plus réalistes possibles (sous réserve de modifications), mais ne peuvent promettre l'exactitude, la complétude et l'actualité des informations répandu sur son site, qu'elles soient de son fait ou non. En conséquence, l'utilisateur reconnaît se servir des informations données (à titre indicatif, non exhaustives et susceptibles d'évoluer) sous sa responsabilité exclusive.</p>
+    
+    <p style="text-align:justify"></p>
+    <br />
+    <p style="text-align:justify"><strong>3. Accessibilité Numérique :</strong><br />
+    <br />
+    Le site suivant ticket+.tg est principalement accessible aux usagés 24/7, sauf en cas de maintenance, programmée ou non, pour les besoins de  mise à jour ou en cas de force majeure. En cas d'impossibilité d'accès au service, ticket+.tg s'engage à faire de son mieux pour rétablir l'accès au site suivant et s'efforcera alors de communiquer au préalable aux utilisateurs les dates et heures de l'interruption. N'étant soumis qu'à une redevance de moyen, ticket+.tg ne pourrait être designé pour responsable de tout dommage, peu importe la nature.</p>
+    
+    <p style="text-align:justify"></p>
+    <br />
+    <p style="text-align:justify"><strong>4. Droit de propriété intellectuelle :</strong><br />
+    <br />
+    AGBEKPENOU Marius est le propriétaire unique de l'intégralité des droits de propriété intellectuelle ou détient les droits et autorisations d'usage sur tous les éléments accessibles sur le site suivant, tant structurellement que sur les articles et textes, , graphismes, icônes, bande sonore, logiciels…<br />
+    Toute reproduction même partielle du site internet ticket+.tg, représentation, modification, mis à jour totale ou partielle de l'un  de ces éléments, peu importe le  moyen ou la technique utilisé, est interdite, sauf autorisation  préalable de AGBEKPENOU Marius, directeur du site suivant à l'email : ****, à défaut elle sera considérée comme constitutive d'une contrefaçon et passible de poursuite conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+    Toute reproduction site internet ticket+.tg, affichage, edition, mis à jour totale ou partielle de l'un quelconque de ces éléments, peu importe le  moyen ou le procédé utilisé, est formellementinterdite, excepté acceptation écrite préalable de AGBEKPENOU Marius, propriétaire du site suivant à l'email : ****, à défaut elle sera considérée de la même manière qu'une contrefaçon étant passible de poursuite selon les dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+    
+    <p style="text-align:justify"><br />
+    <strong>5. Liens hypertextes et cookies :</strong><br />
+    <br />
+    Le site suivant ticket+.tg contient différents URL hypertextes vers différents sites web (partenaires, sites sources .) mis en ligne avec la validation de AGBEKPENOU Marius<a href="https://www.generateur-de-mentions-legales.com/" style="text-decoration:none"><font color="#000000">.</font></a> Néanmoins, AGBEKPENOU Marius n'a pas la possibilité de controler l'intégralité des élements des différents sites  visités et décline donc l'entière responsabilité de ce fait concernant les risques de contenus illicites.<br />
+    L'utilisateur est donc averti que au cours de ses visites sur le site suivant ticket+.tg, un ou différents cookies sont capable de s'intégrer automatiquement sur son ordinateur grâce à son logiciel de navigation. Un cookie c'est un bloc de donnée qui ne permettra jamais d'identifier l'utilisateur, mais qui sauvegarde des renseignements concernant la navigation de celui-ci sur le site web. <br />
+    La configration du logiciel de navigation tolère de renseigner de la compagnie de cookie et potentiellement, de pouvoir  rejeter de la procédure décrite à cette adresse  : <a href="https://www.ticket+.tg">www.ticket+.tg</a>. L'utilisateur à la possibilité néanmoins de configurer le navigateur de son pc afin de refuser la mise en place des cookies, en sachant que le refus de la mise en place d'un cookie pourrait potentiellement entraîner l'incapacité d'accéder à certains secteurs. Concernant tout blocage des cookies, cherchez dans le outil de recherche : blocage des cookies et appliquez les enseignements en rapport avec votre version de navigateur.</p>
+    
+    <strong>6. Protection des personnes et biens - Utilisation des données personnelles :</strong><br />
+    <br />
+    En France, les données personnelles sont par exemple protégées par la loi n° 78-87 du 6 janvier 1978 , la loi n° 2004-801 datant du 6 août 2004, l'article L. 226-13 du Code pénal et la Directive Européenne datant du 24 octobre 1995.</p>
+    
+    <p style="text-align:justify">Sur le site interne ticket+.tg, AGBEKPENOU Marius ne aucune information personnelles ( suivant l'article 4 loi n°78-17 du 06 janvier 1978) concernant l'utilisateur que pour le besoin de divers services offerts par le site interne ticket+.tg. L'usager donne les données en toute connaissance de cause, particulièrement lorsqu'il effectue de lui-même à la saisie. Il est donc expliqué à l'usagé du site web ticket+.tg l'obligation ou non de rentrer ces données.<br />
+    En conformité avec les agencements des articles 38 et autres de la loi 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux différentes libertés, tout utilisateur possède un droit d'accès aux informations, de rectification, de suppression et d'opposition à toutes les données personnelles qui le concerne. Pour l'utiliser, envoyez une demande à ticket+.tg par mail : <strong><a href="mailto:****?subject=Contact à partir des mentions légales via le site ticket+.tg">****</a></strong> ou via écrit  signée, jointe d'une copie de pièce d'identité ainsi qu'une signature du propriétaire du document, en mentionnant l'adresse à laquelle une réponse devra être envoyée.</p>
+    
+    <p style="text-align:justify">Aucune information personnelle de l'utilisateur du site interne ticket+.tg n'est publiée à l'insu de l'utilisateur, échangée, transférée, cédée sur n'importe quel support à des tiers. Uniquement l'hypothèse de vente du site ticket+.tg et de ses droits en autorise AGBEKPENOU Marius à céder les  informations personnelles à l' acquéreur qui serait donc lui-même tenu à la même nécéssité de conservation et de modification des données relative à l'utilisateur du site interne ticket+.tg.<br />
+    Le site web ticket+.tg est conforme avec le RGPD consulter notre politique RGPD.</p>
+    
+    <p style="text-align:justify">Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant ainsi la directive 96/9 du 11 mars 1996 concernant la protection juridique des bases de données(BDD).</p>
+    
+
+<!-- footer debut-->
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12">
+                <h6 class="text-uppercase font-weight-bold">Informations</h6>
+                <p><a class="condition" href="#">Conditions Legales</a></p>
+                <p>Informations Generales</p>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <h6 class="text-uppercase font-weight-bold">Contact</h6>
+            <p>Lome, TOGO
+            <br/>christ124587@live.fr
+            <br/>+228 90 00 29 18
+            <br/>+228 92 36 69 24</p>
+        </div>
+    </div>
+    <div class="footer-copyright text-center">© 2023 Copyright: Ticket+.tg</div>
+</footer>
+<!-- footer fin-->
+        
+    </body>
+    
+</html>
